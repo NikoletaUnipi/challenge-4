13422b712b512643edd28a1a0561e9480f85480d
Π19028 <nikoletaxvs@gmail.com>
2021-10-28 16:55:41 +0300

    added stuff

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace challenge_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine(GetVowelCount("ask"));
+        }
+        public static int GetVowelCount(string str)
+        {
+           
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+           
+            return str.ToList().Count((x => vowels.Contains(x) == true)); ;
+        }
+    }
+}
